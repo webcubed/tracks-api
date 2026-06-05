fe04f8fa8eadc924b2e064c3330bfeb17d098667
webcubed <77589042+webcubed@users.noreply.github.com>
2026-06-04 22:25:13 -0400

    await is ok i suppose

--- scripts/convert.ts
+++ scripts/convert.ts
@@ -14,19 +14,15 @@ if (!fs.existsSync(outputDir)) {
 
 const feeds = fs.readdirSync(path.join(rawDir));
 
-const promises = [];
 for (const feed of feeds) {
 	const currentPath = path.join(rawDir, feed);
 	const files = fs.readdirSync(currentPath);
 	for (const file of files) {
 		const fileName = path.parse(file).name;
-		promises.push(
-			connection.run(`
+		// eslint-disable-next-line no-await-in-loop
+		await connection.run(`
 	COPY (
 		SELECT * FROM read_csv_auto('${path.join(currentPath, fileName)}.txt', all_varchar=true)
-	) TO '${path.join(outputDir, `${feed}_${fileName}.parquet`)}' (FORMAT PARQUET);`)
-		);
+	) TO '${path.join(outputDir, `${feed}_${fileName}.parquet`)}' (FORMAT PARQUET);`);
 	}
 }
-
-await Promise.all(promises);
