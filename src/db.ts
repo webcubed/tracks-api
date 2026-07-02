8e9096724b91b948589a4641008bb330b238179f
webcubed <webcubed@proton.me>
2026-07-02 17:55:03 -0400

    create a duckdb instance

--- src/db.ts
+++ src/db.ts
@@ -1,1 +1,19 @@
+import type { DuckDBConnection } from "@duckdb/node-api";
 import { DuckDBInstance } from "@duckdb/node-api";
+
+let connection: DuckDBConnection;
+
+export async function getDb() {
+	if (connection) return connection;
+	console.log("init'ing db");
+	const instance = await DuckDBInstance.create(":memory:");
+	connection = await instance.connect();
+	await connection.run(`
+		INSTALL httpfs
+		LOAD httpfs
+		INSTALL json
+		LOAD json
+	`);
+	console.log("db up");
+	return connection;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+app.use("*", cors());
+app.get("/", (context) => context.text("ok"));
 
+serve({
+	fetch: app.fetch,
+	port: 3000,
+	hostname: "0.0.0.0",
+});
+
+console.log("server up on port 3000");
 export default app;
